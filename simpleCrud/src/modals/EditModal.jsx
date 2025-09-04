873d2e3a90fc4ed5e24b5a44eac841650cef902f
Joshua-1101 <111673587+Joshua-1101@users.noreply.github.com>
2025-09-05 03:29:05 +0800

    Added: - SweetAlert2 - Confirmation modals

--- simpleCrud/src/modals/EditModal.jsx
+++ simpleCrud/src/modals/EditModal.jsx
@@ -1,41 +1,81 @@
 import React, { useState } from "react";
-
-function EditModal({ editId, onClose, onReload, onqueuser, onquelastname }) {
+import Swal from "sweetalert2";
+import { TypingDotsLoader } from "react-loaderkit";
+function EditModal({
+  editId,
+  onClose,
+  onReload,
+  onqueuser,
+  onquelastname,
+  onComplete,
+}) {
   const [username, setUsername] = useState(onqueuser);
   const [lastname, setLastname] = useState(onquelastname);
+  const [isLoading, setLoading] = useState(false);
   const handleSubmit = async (e) => {
     e.preventDefault();
+   
+
     try {
-      const res = await fetch("http://localhost:5000/api/editUser", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          newUserName: username,
-          newLastName: lastname,
-          userID: editId,
-        }),
+      const result = await Swal.fire({
+        title: "Do you want to save the changes?",
+        showDenyButton: true,
+        showCancelButton: true,
+        confirmButtonText: "Save",
+        denyButtonText: `Don't save`,
       });
+      setLoading(true);
+      if (result.isConfirmed) {
+        const res = await fetch("http://localhost:5000/api/editUser", {
+          method: "PATCH",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            newUserName: username,
+            newLastName: lastname,
+            userID: editId,
+          }),
+        });
+        
+        if (!res.ok) {
+          await Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Something went wrong!",
+          });
+        } else {
+          const data = await res.json();
+          console.log("Edit response:", data);
 
-      const data = await res.json(); // optional: log the response
-      console.log("Edit response:", data);
+          onClose();
+          onReload();
+          onComplete();
+          setUsername("");
+          setLastname("");
 
-      if (res.ok) {
-        onClose(); // ✅ close modal
-        onReload(); // ✅ refresh parent data
-        setUsername("");
-        setLastname("");
-      } else {
-        alert("Failed to update: " + (data.error || data.message));
+          await Swal.fire("Saved!", "", "success");
+        }
+      } else if (result.isDenied) {
+        await Swal.fire("Changes are not saved", "", "info");
       }
     } catch (error) {
       console.error("Error while editing user:", error);
+      await Swal.fire("Error!", "Something went wrong while saving.", "error");
+      onReload();
+      onClose();
+    } finally {
+      setLoading(false);
+      
     }
   };
+
   return (
-    <div className="absolute z-100 bg-gray-500 h-screen w-screen flex flex-col justify-center items-center gap-5">
-      <div className="p-2 bg-gray-200 flex justify-center items-center flex-col shadow-gray-600 shadow-2xl rounded-sm">
+    <div className="absolute z-100 backdrop-blur-2xl h-screen w-screen flex flex-col justify-center items-center gap-5">
+      <div className="p-2 bg-gray-200 min-w-90 flex justify-center items-center flex-col shadow-gray-600 shadow-2xl rounded-sm">
+        <h1 className="bg-gray-500 w-full text-center text-white text-2xl">
+          Update User
+        </h1>
         <form onSubmit={handleSubmit} className="w-full flex flex-col gap-3">
           <div className="flex flex-col">
             <label htmlFor="username">Username</label>
@@ -61,7 +101,15 @@ function EditModal({ editId, onClose, onReload, onqueuser, onquelastname }) {
             type="submit"
             className="bg-blue-500 w-full rounded-sm py-2 text-white hover:bg-blue-700"
           >
-            Save
+            {isLoading ? (
+              <span className="flex justify-center items-center gap-2">
+                Saving
+                <TypingDotsLoader size={40} color="white" speed={1} />
+              </span>
+              
+            ) : (
+              "Save"
+            )}
           </button>
           <button
             type="button"
--- simpleCrud/src/pages/LoginPage.jsx
+++ simpleCrud/src/pages/LoginPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import EditModal from "../modals/EditModal";
-
+import Swal from "sweetalert2";
+import { TypingDotsLoader } from "react-loaderkit";
 function LoginPage() {
   const [username, setUsername] = useState("");
   const [lastname, setLastname] = useState("");
@@ -11,11 +12,16 @@ function LoginPage() {
   // handle on que
   const [onQueUser, setOnQueUser] = useState("");
   const [onQueLastName, setOnQueLastName] = useState("");
+  const [complete, setComplete] = useState(false);
+  const [loading, setLoading] = useState(null);
+  const [fetchStatus, setFetchStatus] = useState(null);
   //handle login
-  const handleLogin = async (e) => {
+  const handleAdd = async (e) => {
     e.preventDefault();
+    setLoading("add");
+
     try {
-      await fetch("http://localhost:5000/api/addUser", {
+      const res = await fetch("http://localhost:5000/api/addUser", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -23,25 +29,59 @@ function LoginPage() {
         body: JSON.stringify({ userName: username, lastName: lastname }),
       });
 
-      alert(username + " " + lastname + " has been added to database.");
-      setReload(prev => !prev);
-      setUsername("");
-      setLastname("");
+      if (res.ok) {
+        Swal.fire({
+          title: `${username} ${lastname} has been added to the database.`,
+          icon: "success",
+          draggable: false,
+        });
+        
+        setUsername("");
+        setLastname("");
+      } else {
+        Swal.fire({
+          icon: "error",
+          title: "Oops...",
+          text: "Something went wrong!",
+        });
+      }
     } catch (error) {
       console.error("Something went wrong...", error);
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: "Network error or server is down!",
+      });
+    } finally {
+      setLoading(null);
+      setReload((prev) => !prev);
     }
   };
 
   //handle fetch
   useEffect(() => {
+    setFetchStatus("fetching");
     const fetchUser = async () => {
       try {
         const res = await fetch("http://localhost:5000/api/getUsers");
+
+        if (!res.ok) {
+          setFetchStatus("fetchFailed");
+          return;
+        }
+
         const data = await res.json();
         console.log("Server response:", data);
-        setUsers(data.results);
+
+        if (!data.results || data.results.length === 0) {
+          setFetchStatus("empty");
+        } else {
+          setUsers(data.results);
+          setFetchStatus(null); 
+        }
       } catch (error) {
         console.error("Fetch error:", error);
+        setFetchStatus("fetchFailed");
       }
     };
 
@@ -50,21 +90,59 @@ function LoginPage() {
 
   //handle Delete
   const handleDelete = async (id) => {
-    if (!window.confirm("Are you sure you want to delete this user?")) return;
-    try {
-      await fetch("http://localhost:5000/api/deleteUser", {
-        method: "DELETE",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ userID: id }),
+    const result = await Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+      showLoaderOnConfirm: true,
+      allowOutsideClick: () => !Swal.isLoading(),
+      preConfirm: async () => {
+        try {
+          const response = await fetch("http://localhost:5000/api/deleteUser", {
+            method: "DELETE",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ userID: id }),
+          });
+
+          if (!response.ok) {
+            throw new Error("Delete failed");
+          }
+
+          return true; // success
+        } catch (error) {
+          Swal.showValidationMessage(`Request failed: ${error}`);
+          setReload((prev) => !prev);
+          return false;
+        }
+      },
+    });
+
+    if (result.isConfirmed) {
+      await Swal.fire({
+        title: "Deleted!",
+        text: "Your file has been deleted.",
+        icon: "success",
       });
-      setReload(prev => !prev)
-    } catch (error) {
-      console.error("Something went wrong.", error);
+      setReload((prev) => !prev);
     }
   };
 
+  useEffect(() => {
+    if (complete) {
+      Swal.fire({
+        title: onQueUser + " " + onQueLastName + " has been updated.",
+        icon: "success",
+        draggable: false,
+      });
+    }
+    setComplete(false);
+  }, [complete]);
   //handle pagination
   const [currentPage, setCurrentPage] = useState(1);
   const usersPerPage = 5; // show 5 users per page
@@ -77,10 +155,14 @@ function LoginPage() {
   const totalPages = Math.ceil(users.length / usersPerPage);
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center gap-5">
-        <h1 className="text-3xl w-full bg-gray-400 text-center py-2 font-semibold text-white">Simple Crud</h1>
-      <div className="p-2 bg-gray-200 flex justify-center items-center flex-col shadow-gray-600 shadow-2xl rounded-sm">
-        <h1 className="bg-gray-500 w-full text-center text-white text-2xl">Add User</h1>
-        <form onSubmit={handleLogin} className="w-full flex flex-col gap-3">
+      <h1 className="text-3xl w-full bg-blue-400 text-center py-2 font-semibold text-white">
+        Simple Crud
+      </h1>
+      <div className="p-2 min-w-90 bg-gray-200 flex justify-center items-center flex-col shadow-gray-600 shadow-lg rounded-sm">
+        <h1 className="bg-gray-500 w-full text-center text-white text-2xl">
+          Add User
+        </h1>
+        <form onSubmit={handleAdd} className="w-full flex flex-col gap-3">
           <div className="flex flex-col">
             <label htmlFor="username">Username</label>
             <input
@@ -89,6 +171,7 @@ function LoginPage() {
               name="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
+              required
             />
           </div>
           <div className="flex flex-col">
@@ -99,26 +182,34 @@ function LoginPage() {
               name="lastame"
               value={lastname}
               onChange={(e) => setLastname(e.target.value)}
+              required
             />
           </div>
           <button
             type="submit"
             className="bg-blue-500 w-full rounded-sm py-2 text-white hover:bg-blue-700"
           >
-            Submit
+            {loading === "add" ? (
+              <span className="flex justify-center items-center gap-2">
+                Adding
+                <TypingDotsLoader size={40} color="white" speed={1} />
+              </span>
+            ) : (
+              "Add"
+            )}
           </button>
         </form>
       </div>
       {/* fetch data */}
-      <div className="p-2 bg-gray-200 flex justify-center items-center flex-col shadow-gray-600 shadow-2xl rounded-sm">
-        <table className="min-w-full divide-y divide-gray-200">
+      <div className="p-2 w-full h-full bg-gray-200 flex items-center flex-col shadow-gray-600 shadow-2xl rounded-sm">
+        <table className="min-w-full h-full  divide-y divide-gray-200">
           <thead className="bg-gray-50">
             <tr>
               <th
                 scope="col"
                 className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
               >
-                Username
+                Firstname
               </th>
               <th
                 scope="col"
@@ -136,88 +227,123 @@ function LoginPage() {
             </tr>
           </thead>
           <tbody className="bg-white divide-y divide-gray-200">
-            {currentUsers.map((user, index) => (
-              <tr key={index}>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="flex items-center">
-                    <div className="ml-4">
-                      <div className="text-sm font-medium text-gray-900">
-                        {user.userName}
+            {fetchStatus === "empty" ? (
+              <td colSpan={3} className="w-full text-center text-2xl">
+                No data yet.
+              </td>
+            ) : fetchStatus === "fetchFailed" ? (
+              <td colSpan={3} className="w-full text-center">
+                <p className="text-3xl font-semibold">Failed to fetch 😕</p>
+                <p className="text-xl">Network error or server is down.</p>
+                <button
+                  className="py-1 px-4 mt-6 rounded-sm bg-red-500 border-gray-900 border-2 hover:bg-red-700 text-white text-lg"
+                  onClick={() => setReload(!reload)}
+                >
+                  Retry
+                </button>
+              </td>
+            ) : fetchStatus === "fetching" ? (
+              <td colSpan={3} className="text-2xl">
+                <div className="w-full flex justify-center items-center gap-2">
+                  Fetching Data
+                  <TypingDotsLoader size={40} color="#111825" speed={1} />
+                </div>
+              </td>
+            ) : fetchStatus === null ? (
+              currentUsers.map((user, index) => (
+                <tr key={index} className="hover:bg-gray-200">
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="flex items-center">
+                      <div className="ml-4">
+                        <div className="text-sm font-medium text-gray-900">
+                          {user.userName}
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <div className="text-sm font-medium text-gray-900">
-                    {user.lastName}
-                  </div>
-                </td>
-                <td>
-                  <button
-                    onClick={() => {
-                      setEditId(user.useID);
-                      setShowEdit(true);
-                      setOnQueUser(user.userName);
-                      setOnQueLastName(user.lastName);
-                    }}
-                    className="text-green-500 hover:underline"
-                  >
-                    Edit
-                  </button>
-                </td>
-                <td>
-                  <button
-                    onClick={() => handleDelete(user.useID)}
-                    className="text-red-500 hover:underline"
-                  >
-                    Delete
-                  </button>
-                </td>
-              </tr>
-            ))}
+                  </td>
+                  <td className="px-6 py-4 whitespace-nowrap">
+                    <div className="text-sm font-medium text-gray-900">
+                      {user.lastName}
+                    </div>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => {
+                        setEditId(user.useID);
+                        setShowEdit(true);
+                        setOnQueUser(user.userName);
+                        setOnQueLastName(user.lastName);
+                        setLoading("update");
+                      }}
+                      className="text-green-500 hover:underline"
+                    >
+                      Edit
+                    </button>
+                  </td>
+                  <td>
+                    <button
+                      onClick={() => handleDelete(user.useID)}
+                      className="text-red-500 hover:underline"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              ))
+            ) : null}
           </tbody>
         </table>
         {/* pagination */}
-        <div style={{ marginTop: "10px", display: "flex", gap: "5px" }}>
-          <button
-            onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
-            disabled={currentPage === 1}
-          >
-            Prev
-          </button>
 
-          {Array.from({ length: totalPages }, (_, i) => (
-            <button
-              key={i + 1}
-              onClick={() => setCurrentPage(i + 1)}
-              style={{
-                padding: "5px 10px",
-                border: "1px solid #ccc",
-                borderRadius: "4px",
-                background: currentPage === i + 1 ? "blue" : "lightgray",
-                color: currentPage === i + 1 ? "white" : "black",
-                cursor: "pointer",
-              }}
-            >
-              {i + 1}
-            </button>
-          ))}
+        <div className="w-full bg-white p-2 flex justify-center items-center gap-2 mt-2">
+          {fetchStatus === null && (
+            <>
+              <button
+                onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+                disabled={currentPage === 1}
+                className="hover:underline disabled:opacity-50"
+              >
+                Prev
+              </button>
 
-          <button
-            onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
-            disabled={currentPage === totalPages}
-          >
-            Next
-          </button>
+              {Array.from({ length: totalPages }, (_, i) => (
+                <button
+                  key={i + 1}
+                  onClick={() => setCurrentPage(i + 1)}
+                  style={{
+                    padding: "5px 10px",
+                    border: "1px solid #ccc",
+                    borderRadius: "4px",
+                    background: currentPage === i + 1 ? "blue" : "lightgray",
+                    color: currentPage === i + 1 ? "white" : "black",
+                    cursor: "pointer",
+                  }}
+                >
+                  {i + 1}
+                </button>
+              ))}
+
+              <button
+                onClick={() =>
+                  setCurrentPage((p) => Math.min(p + 1, totalPages))
+                }
+                disabled={currentPage === totalPages}
+                className="hover:underline disabled:opacity-50"
+              >
+                Next
+              </button>
+            </>
+          )}
         </div>
       </div>
       {showEdit && (
         <EditModal
           editId={editID}
           onClose={() => setShowEdit(false)}
-          onReload={() => setReload(prev => !prev)}
+          onReload={() => setReload((prev) => !prev)}
           onqueuser={onQueUser}
           onquelastname={onQueLastName}
+          onComplete={() => setComplete(true)}
         />
       )}
     </div>
